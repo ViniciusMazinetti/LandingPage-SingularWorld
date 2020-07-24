cec037a2fb5b324135ed4adab3f7d2542010eba0
ViniciusMazinetti <vinicius_mazinetti@hotmail.com>
2020-07-24 17:26:59 -0300

    Explore mask removido

--- js/home.js
+++ js/home.js
@@ -39,14 +39,5 @@ function typpingText(){
 
 }
 
-
-const mountain = document.querySelector(".montanha");
-
-()=>{
-    var value = 905 + window.scrollY;
-
-    mountain.style.top = value + 'px'
-});
-
-
 typpingText();
+
