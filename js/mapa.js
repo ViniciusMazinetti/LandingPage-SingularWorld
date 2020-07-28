5fd9d112228b21850ffe699cad598075f56d1f10
ViniciusMazinetti <vinicius_mazinetti@hotmail.com>
2020-07-28 18:52:41 -0300

    Funcionando o zoom e hover (fica preto e mostra o nome do País)

--- js/mapa.js
+++ js/mapa.js
@@ -1,3 +1,37 @@
-const country = document.querySelector(".paises").children
+const svg = document.querySelector(".paises");
+const zoomIn = document.querySelector("#zoomIn");
+const zoomOut = document.querySelector("#zoomOut");
+let tamanho = 100;
 
-console.log(country);
+function addLabel(){
+    
+    for(let i=0;i<svg.children.length;i++){
+        let countryName = svg.children[i].getAttribute("data-name");
+        svg.children[i].classList.add("pais");
+        svg.children[i].innerHTML = `<title>${countryName}</title>`;
+    }
+}
+function waitButton(){
+    zoomIn.addEventListener("click",()=>{
+        zoom(50);
+    });
+
+    zoomOut.addEventListener("click",()=>{
+        zoom(-50);
+
+    });
+}
+
+function zoom(value){
+    tamanho+=value;
+    if(tamanho<100){
+        tamanho=100;
+    }
+    svg.style.width = tamanho+"vw";
+    svg.style.height = tamanho+"vh";
+    
+}
+
+
+waitButton()
+addLabel();
