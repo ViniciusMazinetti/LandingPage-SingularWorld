0ba0da0f955f02a30223c910ee594b38fb10f9ff
HenriqueAkira <henrique.yasuda@hotmail.com>
2020-07-29 00:58:12 -0300

    Animacao do zoom

--- js/mapa.js
+++ js/mapa.js
@@ -1,37 +1,67 @@
-const svg = document.querySelector(".paises");
-const zoomIn = document.querySelector("#zoomIn");
-const zoomOut = document.querySelector("#zoomOut");
-let tamanho = 100;
+const country = document.querySelector(".paises").children
 
-function addLabel(){
+function createA (){
     
-    for(let i=0;i<svg.children.length;i++){
-        let countryName = svg.children[i].getAttribute("data-name");
-        svg.children[i].classList.add("pais");
-        svg.children[i].innerHTML = `<title>${countryName}</title>`;
+    for(let i = 0; i < country.length; i++){
+        let name = country[i].getAttribute('data-name');
+        const a = document.createElement("a");
+        country[i].classList.add('active');
+        country[i].innerHTML = '<title>'+name+'</title';
     }
 }
-function waitButton(){
-    zoomIn.addEventListener("click",()=>{
-        zoom(50);
-    });
 
-    zoomOut.addEventListener("click",()=>{
-        zoom(-50);
+const inButton = document.querySelector(".upButton");
+const outButton = document.querySelector(".downButton");
+const map = document.querySelector(".paises");
+let num = 100;
+let cIn = 1;
+let cOut = 1;
+var requestId;
 
-    });
-}
-
-function zoom(value){
-    tamanho+=value;
-    if(tamanho<100){
-        tamanho=100;
+function zoomIn(){
+    num += 1.5;
+    if (num >= 300) {
+        num = 300;
+    }else{
+        map.style.width = num + '%';
+        map.style.height = num + '%';
     }
-    svg.style.width = tamanho+"vw";
-    svg.style.height = tamanho+"vh";
-    
+    if (cIn <= 10){
+        requestId = requestAnimationFrame(zoomIn);
+    } else{
+        cancelAnimationFrame(requestId);
+        cIn = 1;
+    }
+    cIn++;
 }
 
+function zoomOut(){
+    num -= 1.5;
+        if (num <= 100) {
+            num = 100;
+            cancelAnimationFrame(requestId);
+        }else{
+            map.style.width = num + '%';
+            map.style.height = num + '%';
+        }
+        if (cOut <= 10){
+            requestId = requestAnimationFrame(zoomOut);
+        } else{
+            cancelAnimationFrame(requestId);
+            cOut = 1;
+        }
+        cOut++;
+}
 
-waitButton()
-addLabel();
+function waitZoom(){
+    outButton.addEventListener('click', () => {
+        zoomOut();
+    })
+    inButton.addEventListener('click', () => {
+        zoomIn();
+    })
+}
+
+waitZoom();
+createA();
+
