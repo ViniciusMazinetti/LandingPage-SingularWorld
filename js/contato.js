a18d22927a2dc537cbaa905c69d9efcbc5f04c5b
ViniciusMazinetti <vinicius_mazinetti@hotmail.com>
2020-08-06 16:04:19 -0300

    Contatos finalizado

--- js/contato.js
+++ js/contato.js
@@ -6,12 +6,15 @@ const options = {
     cache: 'default'
 }
 
+let cidade;
+let estado;
+
 function getEstados(){
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados",options)
     .then(resultado => {resultado.json()
         .then(data =>{
             data.forEach(element => {
-                estados.innerHTML += `<option value = ${element.id}>${element.nome}</option>`;
+                estados.innerHTML += `<option id = ${element.nome} value = ${element.id}>${element.nome}</option>`;
             });
         })})
     .catch(error=>{console.log("Erro")});
@@ -19,16 +22,48 @@ function getEstados(){
 
 function getCidades(){
     estados.addEventListener("change",(e)=>{
-        fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${e.target.value}/municipios`,options)
-        .then(resultado => {resultado.json()
-            .then(data => {
-                data.forEach(element => {
-                    cidades.innerHTML += `<option value = ${element.nome}>${element.nome}</option>`;
-                })
-            })})
-        .catch(error=>{console.log("Erro")});
+        eraseCidade();
+        console.log(e.target.value);
+        if (e.target.value == 0){
+            cidades.setAttribute("disabled",true);   
+        } else {
+            fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${e.target.value}/municipios`,options)
+            .then(resultado => {resultado.json()
+                .then(data => {
+                    data.forEach(element => {
+                        cidades.innerHTML += `<option value = ${element.nome}>${element.nome}</option>`;
+                    })
+                    cidades.removeAttribute("disabled");
+                })})
+            .catch(error=>{console.log("Erro")});
+        }
+        cidades.addEventListener("change",(e)=>{
+            cidade = e.target.value;
+        });      
     });
+}
+
+function eraseCidade(){
+    for (i=0 ; i<cidades.length ; i++){
+        cidades.innerHTML = "<option>Cidades</option>";
+    }
+}
+
+
+function whatsapp(){
+    document.querySelector(".enviar").setAttribute("onclick","abrirLink()");
+}
+
+function abrirLink(){
+    let number = "5512997548653";
+    let nome = document.querySelector("#nome").value;
+    let text = document.querySelector("#mensagem").value;
+    let msg = `Ola eu sou ${nome}, moro em ${cidade}\n${text}`;
+
+    let target = `https://api.whatsapp.com/send?phone=${encodeURIComponent(number)}&text=${encodeURIComponent(msg)}`;
+    window.open(`${target}`);
 }
 
 getEstados();
 getCidades();
+whatsapp();
